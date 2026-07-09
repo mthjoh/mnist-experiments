db5399b794d2202818a61d492cc6fc77d9ec33c6
hjoh <hjoh@hjohui-MacBookAir.local>
2026-07-09 10:42:53 +0900

    Affine backward: dx, dW, db with shape-verified derivation

--- layers.py
+++ layers.py
@@ -15,6 +15,23 @@ class Relu:
         out[self.mask] = 0
         return out
 
+
+class Affine:
+    def __init__(self, W, b):
+        self.W=W
+        self.b=b
+        self.x=None
+        self.dW=None
+        self.db=None
+
+    def forward(self, x):
+        self.x=x
+        return (np.dot(x,self.W)+self.b)
+
+    def backward(self, dout):
+        pass
+
+
 if __name__ == '__main__':
     x = np.array([-2,0,3])
     dout = np.array([1.0,1.0,1.0])

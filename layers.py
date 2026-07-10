2dc780d400f71f525857535b8a4107b14488bd31
hjoh <hjoh@hjohui-MacBookAir.local>
2026-07-10 10:09:20 +0900

    Batch-proofed softmax, CEE, and softmaxwithloss.backward

--- layers.py
+++ layers.py
@@ -47,7 +47,7 @@ class SoftmaxWithLoss:
         return self.loss
     
     def backward(self, dout=1):
-        return (self.y-self.t)*dout
+        return (self.y-self.t)*dout/self.t.shape[0]
 
 # if __name__ == '__main__':
 #     x = np.array([-2,0,3])
@@ -60,19 +60,19 @@ class SoftmaxWithLoss:
     
 
 def softmax(x):
-    xmax = np.max(x)
+    xmax = np.max(x, axis=1, keepdims=True)
     xtamed= x-xmax
     expx = np.exp(xtamed)
-    return expx/np.sum(expx)
+    return expx/np.sum(expx, axis=1, keepdims=True)
 
 def cross_entropy_error(y,t):
-    return -np.log(np.sum(t*y)+1e-7)
+    return np.mean(-np.log(np.sum(t*y,axis=1)+1e-7))
 
-# print(cross_entropy_error(np.array(([1.0, 0.0])),np.array(([1.0, 0.0]))))
-# print(cross_entropy_error(np.array(([1.0, 0.0])),np.array(([0.0, 1.0]))))
+# print(cross_entropy_error(np.array(([[1.0, 0.0,0.0],[0.0, 1.0, 0.0]])),
+# np.array(([[1.0, 0.0,0.0],[1.0, 0.0, 0.0]]))))
+# print(cross_entropy_error(np.array(())))
 
-# print(softmax(np.array([2.0, 1.0, 0.1])))
-# print(softmax(np.array([1002.0, 1001.0, 1000.1]))) 
+# print(softmax(np.array([[2.0, 1.0, 0.1],[1002.0, 1001.0, 1000.1]])))
 
 # loss_layer = SoftmaxWithLoss()
 # print(loss_layer.y, loss_layer.t, loss_layer.loss)
